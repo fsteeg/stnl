776eb9be9d7b068c3f14bdc1be0428d10d4667f4
Fabian Steeg <fsteeg@gmail.com>
2006-12-08 13:05:41 +0000

    share

---
+++ de.uni_koeln.spinfo.nelson/src/Suffix.java
@@ -0,0 +1,139 @@
+//
+// When a new tree is added to the table, we step
+// through all the currently defined suffixes from
+// the active point to the end point.  This structure
+// defines a Suffix by its final character.
+// In the canonical representation, we define that last
+// character by starting at a node in the tree, and
+// following a string of characters, represented by
+// first_char_index and last_char_index.  The two indices
+// point into the input string.  Note that if a suffix
+// ends at a node, there are no additional characters
+// needed to characterize its last character position.
+// When this is the case, we say the node is Explicit,
+// and set first_char_index > last_char_index to flag
+// that.
+
+public class Suffix {
+	
+	public int origin_node;
+	public int first_char_index;
+	public int last_char_index;
+	
+	public Suffix( int node, int start, int stop ){
+		this.origin_node = node;
+		this.first_char_index = start;
+		this.last_char_index = stop;
+	}
+	
+	public int explicit(){ 
+		if(first_char_index > last_char_index){
+			return 1;
+		}
+		else{
+			return 0;
+		}
+	}
+	
+	public int implicit(){ 
+		if(last_char_index >= first_char_index){
+			return 1; 
+		}
+		else{
+			return 0;
+		}
+	}
+	
+//	 A suffix in the tree is denoted by a Suffix structure
+//	 that denotes its last character.  The canonical
+//	 representation of a suffix for this algorithm requires
+//	 that the origin_node by the closest node to the end
+//	 of the tree.  To force this to be true, we have to
+//	 slide down every edge in our current path until we
+//	 reach the final node.
+
+	public void canonize()
+	{
+	    if ( this.explicit() != 1 ) {
+	        Edge edge = Application.ukkonen.find( origin_node, Application.t[ first_char_index ] );
+	        int edge_span = edge.last_char_index - edge.first_char_index;
+	        while ( edge_span <= ( last_char_index - first_char_index ) ) {
+	            first_char_index = first_char_index + edge_span + 1;
+	            origin_node = edge.end_node;
+	            if ( first_char_index <= last_char_index ) {
+	               edge = Application.ukkonen.find( edge.end_node, Application.t[ first_char_index ] );
+	                edge_span = edge.last_char_index - edge.first_char_index;
+	            };
+	        }
+	    }
+	}
+	
+//	 This routine constitutes the heart of the algorithm.
+//	 It is called repetitively, once for each of the prefixes
+//	 of the input string.  The prefix in question is denoted
+//	 by the index of its last character.
+	//
+//	 At each prefix, we start at the active point, and add
+//	 a new edge denoting the new last character, until we
+//	 reach a point where the new edge is not needed due to
+//	 the presence of an existing edge starting with the new
+//	 last character.  This point is the end point.
+	//
+//	 Luckily for use, the end point just happens to be the
+//	 active point for the next pass through the tree.  All
+//	 we have to do is update it's last_char_index to indicate
+//	 that it has grown by a single character, and then this
+//	 routine can do all its work one more time.
+	//
+
+	public static void addPrefix( Suffix active, int last_char_index )
+	{
+	    int parent_node;
+	    int last_parent_node = -1;
+
+	    while(true){
+	        Edge edge;
+	        parent_node = active.origin_node;
+
+			//	 Step 1 is to try and find a matching edge for the given node.
+			//	 If a matching edge exists, we are done adding edges, so we break
+			//	 out of this big loop.
+
+	        if ( active.explicit() == 1 ) {
+	            edge = Application.ukkonen.find( active.origin_node, Application.t[ last_char_index ] );
+	            if ( edge.start_node != -1 )
+	                break;
+	        } else { //implicit node, a little more complicated
+	            edge = Application.ukkonen.find( active.origin_node, Application.t[ active.first_char_index ] );
+	            int span = active.last_char_index - active.first_char_index;
+	            if ( Application.t[ edge.first_char_index + span + 1 ] == Application.t[ last_char_index ] )
+	                break;
+	            parent_node = edge.splitEdge( active );
+	        }
+
+			//	 We didn't find a matching edge, so we create a new one, add
+			//	 it to the tree at the parent node position, and insert it
+			//	 into the hash table.  When we create a new node, it also
+			//	 means we need to create a suffix link to the new node from
+			//	 the last node we visited.
+
+	        Edge new_edge = new Edge( last_char_index, Application.n, parent_node );
+	        new_edge.insert();
+	        if ( last_parent_node > 0 )
+	        	Suffixtree.nodes.get( last_parent_node ).suffix_node = parent_node;
+	        last_parent_node = parent_node;
+
+	        //	 This final step is where we move to the next smaller suffix
+	        
+	        if ( active.origin_node == 0 )
+	            active.first_char_index++;
+	        else
+	            active.origin_node = Suffixtree.nodes.get( active.origin_node ).suffix_node;
+	        active.canonize();
+	    }
+	    if ( last_parent_node > 0 )
+	    	Suffixtree.nodes.get( last_parent_node ).suffix_node = parent_node;
+	    active.last_char_index++;  //Now the endpoint is the next active point
+	    active.canonize();
+	}
+}
---
+++ de.uni_koeln.spinfo.nelson/src/Suffixtree.java
@@ -0,0 +1,189 @@
+import java.util.ArrayList;
+
+
+public class Suffixtree {
+	
+	//The maximum input string length this program
+	//will handle is defined here.  A suffix tree
+	//can have as many as 2N edges/nodes.  The edges
+	//are stored in a hash table, whose size is also
+	//defined here.
+	public static int MAX_LENGTH = 1000;
+	public static int HASH_TABLE_SIZE = 2179;  //A prime roughly 10% larger
+	
+	//The array of defined nodes.  The count is 1 at the
+	//start because the initial tree has the root node
+	//defined, with no children. 
+	// ==> class Nodes sets count = 1
+	public static ArrayList<Node> nodes;
+	
+	//	This is the hash table where all the currently
+	//	defined edges are stored.  You can dump out
+	//	all the currently defined edges by iterating
+	//	through the table and finding edges whose start_node
+	//	is not -1.
+		
+	public ArrayList<Edge> edges;
+
+	//	 The validation code consists of two routines.  All it does
+	//	 is traverse the entire tree.  walk_tree() calls itself
+	//	 recursively, building suffix strings up as it goes.  When
+	//	 walk_tree() reaches a leaf node, it checks to see if the
+	//	 suffix derived from the tree matches the suffix starting
+	//	 at the same point in the input text.  If so, it tags that
+	//	 suffix as correct in the GoodSuffixes[] array.  When the tree
+	//	 has been traversed, every entry in the GoodSuffixes array should
+	//	 have a value of 1.
+		//
+	//	 In addition, the BranchCount[] array is updated while the tree is
+	//	 walked as well.  Every count in the array has the
+	//	 number of child edges emanating from that node.  If the node
+	//	 is a leaf node, the value is set to -1.  When the routine
+	//	 finishes, every node should be a branch or a leaf.  The number
+	//	 of leaf nodes should match the number of suffixes (the length)
+	//	 of the input string.  The total number of branches from all
+	//	 nodes should match the node count.
+	//
+
+	char[] currentString;
+	char[] goodSuffixes;
+	char[] branchCount;
+
+	public void validate()
+	{
+	    for ( int i = 0 ; i < Application.n ; i++ )
+	        goodSuffixes[ i ] = 0;
+	    walk_tree( 0, 0 );
+	    int error = 0;
+	    for ( int i = 0 ; i < Application.n ; i++ )
+	        if ( goodSuffixes[ i ] != 1 ) {
+	            System.out.println("Suffix " + i + " count wrong!\n");
+	            error++;
+	        }
+	    if ( error == 0 )
+	        System.out.println("All Suffixes present!\n");
+	    int leaf_count = 0;
+	    int branch_count = 0;
+	    for (int i = 0 ; i < Node.count ; i++ ) {
+	        if ( branchCount[ i ] == 0 )
+	            System.out.println("Logic error on node " + i
+	                 + ", not a leaf or internal node!\n");
+	        else if ( branchCount[ i ] == -1 )
+	            leaf_count++;
+	        else
+	            branch_count += branchCount[ i ];
+	    }
+	    System.out.println("Leaf count : " + leaf_count);
+	    if(leaf_count == ( Application.n + 1 )){
+	    	System.out.println("OK");
+	    }
+	    else{
+	    	System.out.println("Error!");
+	    }
+	    System.out.println("Branch count : " + branch_count);
+	    if(branch_count == (Node.count - 1)){
+	    	System.out.println("OK");
+	    }
+	    else{
+	    	System.out.println("Error!");
+	    }
+	}
+	
+	//	The whole reason for storing edges in a hash table is that it
+	//	makes this function fairly efficient.  When I want to find a
+	//	particular edge leading out of a particular node, I call this
+	//	function.  It locates the edge in the hash table, and returns
+	//	a copy of it.  If the edge isn't found, the edge that is returned
+	//	to the caller will have start_node set to -1, which is the value
+	//	used in the hash table to flag an unused entry.
+		
+	public Edge find( int node, int c )
+	{
+	    int i = hash( node, c );
+	    for ( ; ; ) {
+	        if ( this.edges.get(i).start_node == node )
+	            if ( c == Application.t[ this.edges.get(i).first_char_index ] )
+	                return this.edges.get(i);
+	        if ( this.edges.get(i).start_node == -1 )
+	            return this.edges.get(i);
+	        i = ++i % Suffixtree.HASH_TABLE_SIZE;
+	    }
+	}
+			
+			
+	//		This routine prints out the contents of the suffix tree
+	//		at the end of the program by walking through the
+	//		hash table and printing out all used edges.  It
+	//		would be really great if I had some code that will
+	//		print out the tree in a graphical fashion, but I don't!
+			
+	public void dump_edges( int current_n )
+	{
+		System.out.println(" Start  End  Suf  First Last  String\n");
+		for ( int j = 0 ; j < Suffixtree.HASH_TABLE_SIZE ; j++ ) {
+			//Edge *s = Edges + j;  
+			Edge s = this.edges.get(j);
+			if ( s.start_node == -1 )
+				continue;
+			System.out.println(s.start_node + "  " + s.end_node + "  " 
+					+ Suffixtree.nodes.get(s.end_node).suffix_node + "  " 
+					+ s.first_char_index + "  " + s.last_char_index);
+			int top;
+			if ( current_n > s.last_char_index )
+				top = s.last_char_index;
+			else
+				top = current_n;
+			for ( int l = s.first_char_index ; l <= top; l++ )
+				System.out.println(Application.t[ l ]);
+		}
+	}
+
+	public int walk_tree( int start_node, int last_char_so_far )
+	{
+	    int edges = 0;
+	    for ( int i = 0 ; i < 256 ; i++ ) {
+	        Edge edge = this.find( start_node, i );
+	        if ( edge.start_node != -1 ) {
+	            if ( branchCount[ edge.start_node ] < 0 )
+	                System.out.println("Logic error on node " + edge.start_node);
+	            branchCount[ edge.start_node ]++;
+	            edges++;
+	            int l = last_char_so_far;
+	            for ( int j = edge.first_char_index ; j <= edge.last_char_index ; j++ )
+	                currentString[ l++ ] = Application.t[ j ];
+	            currentString[ l ] = '\0';
+	            if ( walk_tree( edge.end_node, l ) == 1 ) {
+	                if ( branchCount[ edge.end_node ] > 0 )
+	                        System.out.println("Logic error on node " + edge.end_node);
+	                branchCount[ edge.end_node ]--;
+	            }
+	        }
+	    }
+	//
+	//	 If this node didn't have any child edges, it means we
+	//	 are at a leaf node, and can check on this suffix.  We
+	//	 check to see if it matches the input string, then tick
+	//	 off it's entry in the GoodSuffixes list.
+	//
+	    if ( edges == 0 ) {
+	        System.out.println("Suffix : ");
+	        for ( int m = 0 ; m < last_char_so_far ; m++ )
+	            System.out.println(currentString[ m ]);
+	        goodSuffixes[ currentString.length - 1 ]++;
+//	        System.out.println("comparing: " << ( T + Application.n - currentString.length + 1 )
+//	             << " to " << currentString << endl;
+//	        if ( strcmp(T + Application.n - currentString.length + 1, currentString ) != 0 )
+//	            System.out.println("Comparison failure!\n");
+	        return 1;
+	    } else
+	        return 0;
+	}
+	
+	//	Edges are inserted into the hash table using this hashing
+	//	function.
+		
+	public static int hash( int node, int c )
+	{
+	    return ( ( node << 8 ) + c ) % Suffixtree.HASH_TABLE_SIZE;
+	}
+}
