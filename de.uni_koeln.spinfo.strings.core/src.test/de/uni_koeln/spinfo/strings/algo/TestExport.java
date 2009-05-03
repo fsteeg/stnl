41294979ea4d7142363dde3db671489b0473c921
Fabian Steeg <fsteeg@gmail.com>
2009-05-03 23:47:46 +0000

    Updated sample usage

--- de.uni_koeln.spinfo.strings.core/src.test/de/uni_koeln/spinfo/strings/algo/TestExport.java
+++ de.uni_koeln.spinfo.strings.core/src.test/de/uni_koeln/spinfo/strings/algo/TestExport.java
@@ -6,20 +6,24 @@ import de.uni_koeln.spinfo.strings.algo.suffixtrees.DAG;
 import de.uni_koeln.spinfo.strings.algo.suffixtrees.NumericSuffixTree;
 import de.uni_koeln.spinfo.strings.algo.suffixtrees.WordSuffixTree;
 
+/**
+ * Sample creation and export to Graphviz DOT for numeric, char-based and
+ * word-based suffix trees and directed acyclic graphs (DAG).
+ * @author Fabian Steeg (fsteeg)
+ */
 public class TestExport extends TestCase {
     public void testExport() {
-        NumericSuffixTree tree;
-        // ein Baum mit Woertern:
+        /* In the end, every suffix tree is a numeric tree: */
+        NumericSuffixTree<?> tree;
+        /* It can be a word-based suffix tree: */
         tree = new WordSuffixTree(
                 "Ich esse Fisch. Ich esse Fleisch. Ich esse. Ich trinke.");
         tree.exportDot("word-tree.dot");
-        // ein Baum mit Buchstaben:
+        /* Or a char-based suffix tree: */
         tree = new CharSuffixTree("aufgegangen abgegangen aufgehangen");
         tree.exportDot("char-tree.dot");
-
-        NumericSuffixTree wordTree = new WordSuffixTree(
-                "Ich esse Fisch. Ich esse Fleisch. Ich esse. Ich trinke.");
-        DAG wordDag = new DAG(wordTree);
+        /* Any suffix tree can be converted to a DAG: */
+        DAG wordDag = new DAG(tree);
         wordDag.exportDot("word-dag.dot");
     }
 }
